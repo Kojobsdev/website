8596d36162ce6d79172a6fe0afc4b11fc61b425d
studentuser <studentuser@D-10-157-153-231.dhcp4.washington.edu>
2017-05-05 21:44:06 -0700

    Add bootstrap

--- kojobs/settings.py
+++ kojobs/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps'
+    'apps',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
