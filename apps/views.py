413a20449bd6ece596503e04e647615d55edbec8
Will Hwang <sang7239@D-10-157-85-107.dhcp4.washington.edu>
2017-05-05 21:53:11 -0700

    Add register form

--- apps/views.py
+++ apps/views.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def register(request):
+    return render(request, "form/register.html")
 # Create your views here.
